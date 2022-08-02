0346f57be67c8d467a8fa7d784f8c7d8ccc5a42c
ninjadelcodigo <ninjadelcodigo@gmail.com>
2022-08-01 20:52:03 -0500

    1er commit a leccion-18

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
-## JavaScript Moderno - Lección 17 (Alcance de Variables)
+## JavaScript Moderno - Lección 17 (Introducción a funciones)
 
-* como hemos visto se puede re-definir las variables en un bloque de codigo
-* bloques de código anidados
-* Nota: los bloques de código y su alcance en lo que concierne puede ser confusa (repetir lección para entenderlo bien)
+* declaración de función
+* llamar una función declarada
+* expresión de función  (notar el punto y coma al final de este bloque de código)
+* llamar una función expresada
+* esta dos formas son casi iguales con la diferencia que las funciones expresadas son izadas por JavaScript
+* nota: sin embardo, declarar una función después de llamarla es una mala práctica ya que se complica la lecura del código
+* error si el parametro de una función es llamado afuera de la función
+* si no se pasan parametros los parametros asumen el valor 'Undefined'
+* funcion con valores de parametros por definición
+* al llamar una funcion con parametros por defición los parametrós no pasados asumen los valores por definicion
 
 
 **Link al curso traducido en YouTube:** [JavaScript Moderno](https://www.youtube.com/channel/UCuSHTq2yiCY5QBNoEXv8JpA/)
--- sandbox.js
+++ sandbox.js
@@ -1,35 +1,56 @@
 
-// como hemos visto se puede re-definir las variables en un bloque de codigo
+// declaración de función
+function saludar() {
+    console.log('hola!');
+}
 
-let age = 25;
+// llamar una función declarada
+saludar();
+saludar();
 
-// error:
-//let age = 30;
+// expresión de función  (notar el punto y coma al final de este bloque de código)
+const despedirse = function() {
+    console.log('adios!')
+};
 
-// ok
-age = 30;
-if (true) {
-    let age = 40;
-    let name = 'peach';
-    console.log('dentro del 1er bloque de codigo', age, name);
-} 
+// llamar una función declarada
+despedirse();
+despedirse();
+despedirse();
 
-console.log('fuera del bloque de codigo', age, name);
+// esta dos formas son casi iguales con la diferencia que las funciones expresadas son izadas por JavaScript
 
-// bloques de código anidados
-if (true) {
-    let age = 40;
-    let name = 'peach';
-    console.log('dentro del 1er bloque de codigo', age, name);
-    if (true) {
-        let age = 50;
-        let name = 'yoshi';
-        console.log('dentro del 2do bloque de codigo', age, name);
+hablar();
+hablar();
 
-    }
-} 
+function hablar() {
+    console.log('hablando');
+}
 
-console.log('fuera del bloque de codigo', age, name);
+// nota: sin embardo, declarar una función después de llamarla es una mala práctica ya que se complica la lecura del código
 
-// Nota: los bloques de código y su alcance en lo que concierne puede ser confusa (repetir lección para entenderlo bien)
+// funciones con parametros
+const hablarle = function(frase, persona)  {
+    console.log(frase, persona);
+}
 
+// llamar a una función parametrizada
+hablarle('hola', 'peach');
+hablarle('adios', 'mario');
+hablarle('hablando con', 'yoshi');
+
+// error si el parametro de una función es llamado afuera de la función
+// persona = 'peach';
+
+// si no se pasan parametros los parametros asumen el valor 'Undefined'
+hablarle(); 
+
+// funcion con valores de parametros por definición
+const decirle = function(frase = 'hello', persona = 'shaun')  {
+    console.log(frase, persona);
+}
+
+// al llamar una funcion con parametros por defición los parametrós no pasados asumen los valores por definicion
+decirle();
+decirle('hola');
+decirle('adios', 'peach');
